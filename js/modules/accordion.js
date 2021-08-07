73492ec7941963ac3e547f4432ac7f7fdb1af867
Laíse Carvalho <laisecrv@gmail.com>
2021-08-07 13:56:36 -0300

    refactor: refactoring smooth-scroll

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -2,17 +2,17 @@ export default function initAccordion() {
   const accordionList = document.querySelectorAll(
     '[data-anime="accordion"] dt'
   );
-  accordionList[0].classList.add("ativo");
-  accordionList[0].nextElementSibling.classList.add("ativo");
+  accordionList[0].classList.add('ativo');
+  accordionList[0].nextElementSibling.classList.add('ativo');
 
   if (accordionList.length) {
     accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
+      item.addEventListener('click', activeAccordion);
     });
 
     function activeAccordion(event) {
-      this.classList.toggle("ativo");
-      this.nextElementSibling.classList.toggle("ativo");
+      this.classList.toggle('ativo');
+      this.nextElementSibling.classList.toggle('ativo');
     }
   }
 }
