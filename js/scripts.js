a44bc1787ec80d02d0df56615c2fd32ebab814a1
Laíse Carvalho <laisecrv@gmail.com>
2021-08-07 17:22:53 -0300

    refactor: create debounce module and change functions for classes

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,13 @@ import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js';
+import ScrollAnima from './modules/scroll-animacao.js';
+import DropdownMenu from './modules/dropdown-menu.js';
+import MenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
+import Funcionamento from './modules/funcionamento.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -32,10 +33,17 @@ modal.init();
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
 
-initAnimacaoScroll();
-initDropdownMenu();
-initMenuMobile();
-initFuncionamento();
-initFetchBitcoin();
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+const dropdownMenus = new DropdownMenu('[data-dropdown]');
+dropdownMenus.init();
+
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
+const funcionamento = new Funcionamento('[data-semana]', 'aberto');
+funcionamento.init();
+
+fetchAnimais('./animaisapi.json', '.numeros-grid');
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
