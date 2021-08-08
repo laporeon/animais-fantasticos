77d042b1240efca744d16521c6d0200e01ef4de4
Laíse Carvalho <laisecrv@gmail.com>
2021-08-08 15:03:40 -0300

    feat: add slide with animals

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,10 @@ import Tooltip from './modules/tooltip.js';
 import ScrollAnima from './modules/scroll-animacao.js';
 import DropdownMenu from './modules/dropdown-menu.js';
 import MenuMobile from './modules/menu-mobile.js';
-import initFuncionamento from './modules/funcionamento.js';
+import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
-import Funcionamento from './modules/funcionamento.js';
+import SlideNav from './modules/slide.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -47,3 +47,7 @@ funcionamento.init();
 
 fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addControl('.custom-controls');
